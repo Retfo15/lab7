17b9d0a14fb3cc5fe2edd16145de0cf79e62e3bb
Kirill Derevkov <derevkovk@gmail.com>
2024-06-03 19:51:35 +0300

    created new class XmlService

---
+++ WeatherService/XmlService.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Service.h"
+#include "pugixml.hpp"
+#include "pugiconfig.hpp"
+#include <iostream>
+
+using namespace pugi;
+
+class XmlService : public Service {
+public:
+	Weather getWeather(std::string s) override;
+	~XmlService() override {};
+};
